812838d635c8b2c17d17a3e9b29ae8742ed87c7e
Rafa130206 <rafabompadre@gmail.com>
2025-07-31 18:36:45 -0300

    Primeiro commit

---
+++ src/main/java/br/com/rpg/APICampanha/ApiCampanhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpg.APICampanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCampanhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCampanhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rpg/APICampanha/ApiCampanhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rpg.APICampanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCampanhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
